cc4d6aa51e947747db351e5d2f5e78c1eaacab92
Nicolas Braud-Santoni <nicoo@realraum.at>
2017-12-09 22:36:18 +0100

    Make keygen.py comply with PEP8

--- keygen.py
+++ keygen.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-import base64, os
 
+from base64 import b32encode
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives.hashes import Hash, SHA1
+
 
 def onion_keygen():
     return rsa.generate_private_key(
@@ -21,13 +22,15 @@ def onion_name(key):
     )
     hash = Hash(SHA1(), backend=default_backend())
     hash.update(pub_bytes)
-    return base64.b32encode(hash.finalize()[:10]).lower() + b'.onion'
+    return b32encode(hash.finalize()[:10]).lower() + b'.onion'
+
 
 OS_PATH = '/var/lib/tor/onion_service'
 PRIV_KEY_PATH = os.path.join(OS_PATH, 'private_key')
 HOSTNAME_PATH = os.path.join(OS_PATH, 'hostname')
 
 if __name__ == '__main__':
+    import os
     if os.path.exists(PRIV_KEY_PATH):
         with open(PRIV_KEY_PATH, 'rb') as file:
             privkey = serialization.load_pem_private_key(
@@ -44,7 +47,6 @@ if __name__ == '__main__':
                 format=serialization.PrivateFormat.PKCS8,
                 encryption_algorithm=serialization.NoEncryption
             ))
-
 
     hostname = onion_name(privkey)
     if not os.path.exists(HOSTNAME_PATH):
